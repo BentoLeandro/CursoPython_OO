c8c33718be27ddafda6e48a9348cd0affcb6569f
BentoLeandro <leo_lrb90@hotmail.com>
2021-08-22 11:32:55 -0300

    atualização

--- conta.py
+++ conta.py
@@ -1,4 +1,4 @@
-from teste import deposita
+#from teste import deposita
 
 
 class Conta:
@@ -8,6 +8,7 @@ class Conta:
         self.__titular = titular
         self.__saldo = saldo
         self.__limite = limite
+        #self.__codigo_banco = "001"
 
     def extrato(self):
         return print(f"Saldo.: {self.__saldo} do titular.: {self.__titular}")
@@ -15,8 +16,15 @@ class Conta:
     def depositar(self, valor):
         self.__saldo += valor
 
+    def __pode_sacar(self, valor_sacar):
+        valor_disponivel = self.__saldo + self.__limite
+        return valor_sacar <= valor_disponivel
+
     def sacar(self, valor):
-        self.__saldo -= valor 
+        if(self.__pode_sacar(valor)):
+            self.__saldo -= valor 
+        else:
+            print(f"O valor {valor} passou do Limite!")    
 
     def transferir(self, valor, destino):
         self.sacar(valor)
@@ -36,22 +44,39 @@ class Conta:
 
     @limite.setter
     def limite(self, valor):
-        self.__limite = valor           
+        self.__limite = valor      
 
-conta1 = Conta(123, "Bento", 23.34, 2000.00)
-conta2 = Conta(222, "Maria", 100.00, 3000.00)
+    @staticmethod
+    def codigo_banco():
+        return "001" #self.__codigo_banco   
+
+    @staticmethod
+    def codigos_bancos():
+        return {'BB':'001','Caixa':104,'Bradesco':'237'}          
 
 
+from conta import Conta
+
+Conta.codigo_banco()
+Conta.codigos_bancos()
+
+#conta1 = Conta(123, "Bento", 23.34, 2000.00)
+#conta2 = Conta(222, "Maria", 100.00, 3000.00)
+
+
+"""
 print("*"*30)
 print(conta1.saldo)
 print(conta1.titular)
 print(conta1.limite)
 
-conta1.limite = 6000.00
+#conta1.limite = 6000.00
 
 print(conta1.limite)
 print("*"*30)
 
+conta1.sacar(4000.00)
+print(conta1.saldo)
 
 conta2.transferir(30, conta1)
 
@@ -61,4 +86,4 @@ print(conta1.extrato())
 #print(conta.sacar(50))
 #print(conta.extrato())
 print(conta2.extrato())
-
+"""
--- teste.py
+++ teste.py
@@ -1,25 +1,6 @@
-def cria_conta(numero, titular, saldo, limite):
-    conta = {"numero": numero, "titular": titular, "saldo": saldo, "limite": limite}
-    return conta
+from conta import Conta
 
-def deposita(conta, valor):
-    conta["saldo"] += valor
-
-def sacar(conta, valor):
-    conta["saldo"] -= valor
-
-def extrato(conta):
-    print("Saldo é.: {}".format(conta["saldo"]))    
-
-conta = cria_conta(123, "Bento", 123.9, 2000.00)
-
-deposita(conta, 500.00)
-extrato(conta)
-
-sacar(conta, 100)
-extrato(conta)
-
-
-
+print(Conta.codigos_bancos())
+print(Conta.codigo_banco())
 
 
